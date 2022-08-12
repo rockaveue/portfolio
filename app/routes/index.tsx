6114662479afa029ffe7f237502691ac08183e21
Dulguun <rockaveue@gmail.com>
2022-08-12 14:53:08 +0800

    npm to yarn, chakra toggle darkmode

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,23 @@
-import { Box, Heading, HStack, Link as ChakraLink } from '@chakra-ui/react'
+import {
+  Box,
+  Heading,
+  HStack,
+  IconButton,
+  Link as ChakraLink,
+  useColorMode,
+} from '@chakra-ui/react'
 import { Link } from '@remix-run/react'
+import { MoonIcon, SunIcon } from '@chakra-ui/icons'
 
 export default function Index() {
+  const { colorMode, toggleColorMode } = useColorMode()
   return (
     <Box fontFamily="system-ui, sans-serif" lineHeight={1.4}>
+      <IconButton
+        aria-label="toggle-dark"
+        icon={colorMode === 'dark' ? <SunIcon /> : <MoonIcon />}
+        onClick={toggleColorMode}
+      />
       <Heading as="h1" color="blue.400">
         Welcome to Remix with Chakra UI!
       </Heading>
